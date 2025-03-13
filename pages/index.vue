2a9d50359eace5fcae6b9bb83be67ecace95b04d
Muhammad-Imran <muhammad.imran@ki5.co.uk>
2025-03-13 16:30:11 +0500

    changed images with low quality

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <link rel="preload" href="main.css" as="style">
+        <link rel="stylesheet" href="main.css" media="print" onload="this.media='all'">
        <HomePageMainSection />
        <ServicesSection />
        <PortfolioCarousel/>
